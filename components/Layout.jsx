46b9bdfe9f56cf2daf28de1f17d8f3c8565c93d1
Diego Briceño <diegobricep@gmail.com>
2022-03-30 23:56:39 -0500

    Add initial config

---
+++ components/Layout.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>LaHaus</title>
+        <meta
+          name="description"
+          content="¿Buscas una inmobiliaria? Tenemos una buena noticia, somos mucho más. La tecnología y oferta de finca raíz harán más fácil la compra de tu vivienda nueva."
+        />
+        <link rel="preload" href="fonts/silka-regular-webfont.ttf" as="font" crossOrigin="" />
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
