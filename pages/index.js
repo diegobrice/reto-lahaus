5d1c88e278f5d8d9df6ba8b52fa69e6a1df5a518
Diego Briceño <diegobricep@gmail.com>
2022-03-31 00:35:23 -0500

    Add home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '../components/Layout';
+import Card from '../components/Card';
 import useAppContext from '../context/state';
+import SkeletonCard from '../skeletons/SkeletonCard';
 
 export default function Home() {
   const {
@@ -9,7 +11,19 @@ export default function Home() {
 
   return (
     <Layout>
-      <h1>LaHaus</h1>
+      <div className="home">
+        <div className="container">
+          <h1>Hoy se compra casa nueva en LaHaus.</h1>
+          {isLoading && <SkeletonCard />}
+          {list && (
+            <section className="grid">
+              {list.map((item) => {
+                return <Card key={item.id} item={item} />;
+              })}
+            </section>
+          )}
+        </div>
+      </div>
     </Layout>
   );
 }
